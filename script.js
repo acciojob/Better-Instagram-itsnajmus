e379140e5ee9fbf108e6b38a665a7f95f216ed1b
Najmus Sakib <106301767+itsnajmus@users.noreply.github.com>
2023-03-04 15:22:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,41 @@
-//your code here
+const images = document.querySelectorAll('.image');
+
+let dragSrcEl = null;
+
+function handleDragStart(e) {
+  this.style.opacity = '0.4';
+  dragSrcEl = this;
+  e.dataTransfer.effectAllowed = 'move';
+  e.dataTransfer.setData('text/html', this.innerHTML);
+}
+
+function handleDragOver(e) {
+  if (e.preventDefault) {
+    e.preventDefault();
+  }
+  e.dataTransfer.dropEffect = 'move';
+  return false;
+}
+
+function handleDrop(e) {
+  if (e.stopPropagation) {
+    e.stopPropagation();
+  }
+  if (dragSrcEl !== this) {
+    dragSrcEl.innerHTML = this.innerHTML;
+    this.innerHTML = e.dataTransfer.getData('text/html');
+  }
+  return false;
+}
+
+function handleDragEnd(e) {
+  this.style.opacity = '1';
+  images.forEach(img => img.classList.remove('over'));
+}
+
+images.forEach(img => {
+  img.addEventListener('dragstart', handleDragStart, false);
+  img.addEventListener('dragover', handleDragOver, false);
+  img.addEventListener('drop', handleDrop, false);
+  img.addEventListener('dragend', handleDragEnd, false);
+});
